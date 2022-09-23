45c98e8e8f899ff21df1d0b9da516f1b0773a6c1
Devotlig <1756138042@qq.com>
2022-09-24 03:02:46 +0800

    canflyRainbow

---
+++ circlegenerater.py
@@ -0,0 +1,65 @@
+from plangenerater import PlanGenerater
+import math
+class CircleGenerater():
+    Length_Lon = 20037000 #经线的长度
+    Length_equator = 40075020
+    def InitPlanGenerater(self):
+        self.plangenerater = PlanGenerater()
+        
+
+    def __init__(self) -> None:
+        #self.InitPlanGenerater()
+        pass
+    
+    def SetCenter(self,Center_Lat,Center_Lon,Center_AltRel):
+        self.Center_Lat = Center_Lat
+        self.Center_Lon = Center_Lon
+        self.Center_AltRel = Center_AltRel
+        self.Length_ThisLat = math.cos(2*math.pi*Center_Lat/180.0)
+
+    def CircleGetPointList(self,Circle_Rad,Circle_Dis,Circle_theta):
+        self.PointList = [{"Lat":self.Center_Lat,"Lon":self.Center_Lon,"AltRel":self.Center_AltRel+Circle_Rad}]
+
+    def RainbowGetPointList(self,Rainbow_Rad,Rainbow_Dis,Rainbow_Begin_Lat,Rainbow_Begin_Lon,Rainbow_Begin_AltRel,Rainbow_Center_Lat,Rainbow_Center_Lon,Rainbow_Center_AltRel):
+        RainbowPointList = [{"Lat":Rainbow_Begin_Lat,"Lon":Rainbow_Begin_Lon,"AltRel":Rainbow_Begin_AltRel}]
+        Length_thisLat = self.Length_equator*math.cos(Rainbow_Center_Lat*math.pi/90)
+        Rainbow_Lenthground = math.sqrt(math.pow((Rainbow_Begin_Lat-Rainbow_Center_Lat)*self.Length_Lon/360,2)+math.pow((Rainbow_Begin_Lon-Rainbow_Center_Lon)*Length_thisLat/360,2)) # length in ground
+        Rainbow_LengthPoint2Center = Rainbow_Lenthground - Rainbow_Dis
+        
+        while Rainbow_LengthPoint2Center >= 0 :
+            PointElement = {}
+            PointElement["Lat"] = (Rainbow_LengthPoint2Center*Rainbow_Begin_Lat+(Rainbow_Lenthground-Rainbow_LengthPoint2Center)*Rainbow_Center_Lat)/Rainbow_Lenthground
+            PointElement["Lon"] = (Rainbow_LengthPoint2Center*Rainbow_Begin_Lon+(Rainbow_Lenthground-Rainbow_LengthPoint2Center)*Rainbow_Center_Lon)/Rainbow_Lenthground
+            PointElement["AltRel"] = math.sqrt(math.pow(Rainbow_Rad,2) - math.pow(Rainbow_LengthPoint2Center,2))+Rainbow_Center_AltRel
+            RainbowPointList.append(PointElement)
+            print(PointElement)
+            Rainbow_LengthPoint2Center = Rainbow_LengthPoint2Center - Rainbow_Dis
+
+        Rainbow_LengthPoint2Center = Rainbow_LengthPoint2Center + Rainbow_Dis
+
+        while Rainbow_LengthPoint2Center <= Rainbow_Lenthground :
+            PointElement = {}
+            PointElement["Lat"] = (-Rainbow_LengthPoint2Center*Rainbow_Begin_Lat+(Rainbow_Lenthground+Rainbow_LengthPoint2Center)*Rainbow_Center_Lat)/Rainbow_Lenthground
+            PointElement["Lon"] = (-Rainbow_LengthPoint2Center*Rainbow_Begin_Lon+(Rainbow_Lenthground+Rainbow_LengthPoint2Center)*Rainbow_Center_Lon)/Rainbow_Lenthground
+            PointElement["AltRel"] = math.sqrt(math.pow(Rainbow_Rad,2) - math.pow(Rainbow_LengthPoint2Center,2))+Rainbow_Center_AltRel
+            RainbowPointList.append(PointElement)
+            print(PointElement)
+            Rainbow_LengthPoint2Center = Rainbow_LengthPoint2Center + Rainbow_Dis
+
+            
+        return RainbowPointList
+
+    def Run(self,PointList,TargetFile):
+        self.InitPlanGenerater()
+        self.plangenerater.ChangeTakeoffPoint(PointList[0]["Lat"],PointList[0]["Lon"],PointList[0]["AltRel"])
+        for PointElement in PointList:
+            self.plangenerater.AddWaypoint(PointElement["Lat"],PointElement["Lon"],PointElement["AltRel"])
+        self.plangenerater.AddLandtoTakeoffPointCommand()
+        self.plangenerater.GenerateFile(TargetFile)    
+        
+
+
+
+if __name__ == "__main__":
+    circlegenerater = CircleGenerater()
+    circlegenerater.Run(circlegenerater.RainbowGetPointList(50,1,34.12694001,108.82283213,30,34.12710522,108.82293812,10),"testall.plan")
---
+++ plangenerater.py
@@ -0,0 +1,190 @@
+from email.mime import base
+import json
+json.encoder.FLOAT_REPR = lambda x: format(x, '.11f')
+'''
+with open("testwaypoint.plan","r",encoding = "utf-8") as f:
+    word = f.read()
+    
+    #print(word)
+    word_load = json.loads(word)
+    print(word_load)
+    
+    advise = []
+    s = word.split("\n")
+    for single in s:
+        if len(single.split("：")) == 2:
+            advise.append(single.split("：")[1])
+
+
+
+    givetimes = Give_Times(*advise)
+    times = givetimes.run()
+    '''
+class PlanGenerater:
+    BaseFile = {
+        "fileType":"Plan",
+        "geoFence":{
+            "circles":[
+
+            ],
+            "polygons":[
+
+            ],
+            "version":2
+        },
+        "groundStation":"QGroundControl",
+        "mission":{
+            "cruiseSpeed":15,
+            "firmwareType":12,
+            "globalPlanAltitudeMode":1,
+            "hoverSpeed":5,
+            "items":[{
+                "AMSLAltAboveTerrain":None,
+                "Altitude":50,
+                "AltitudeMode":1,
+                "autoContinue":True,
+                "command":22,
+                "doJumpId":1,
+                "frame":3,
+                "params":[
+                    0,
+                    0,
+                    0,
+                    None,
+                    34.12739242,
+                    108.83128143,
+                    50
+                ],
+                "type":"SimpleItem"
+            }
+            ],
+            "plannedHomePosition":[
+                34.12739242,
+                108.83128143,
+                421.61314800000787
+            ],
+            "vehicleType":2,
+            "version":2
+        },
+        "rallyPoints":{
+            "points":[
+
+            ],
+            "version":2
+        },
+        "version":1
+        }
+    BaseItem = {
+                "AMSLAltAboveTerrain":None,
+                "Altitude":50,
+                "AltitudeMode":1,
+                "autoContinue":True,
+                "command":22,    #操作ID
+                "doJumpId":1,    #顺序
+                "frame":3,
+                "params":[
+                    0,
+                    0,
+                    0,
+                    None,
+                    34.12739242,
+                    108.83128143,
+                    50
+                ],
+                "type":"SimpleItem"
+            }
+    def __init__(self) -> None:
+        pass
+    def GetItemNum(self):
+        return len(self.BaseFile["mission"]["items"])
+    def AddCommandItem(self,CommandID,param):
+        pass
+    def ChangeTakeoffPoint(self,Takeoffpoint_Lat,Takeoffpoint_Lon,Takeoffpoint_AltRel):
+        self.Takeoffpoint_Lat = Takeoffpoint_Lat
+        self.Takeoffpoint_Lon = Takeoffpoint_Lon
+        self.Takeoffpoint_AltRel = Takeoffpoint_AltRel
+        self.BaseFile["mission"]["items"][0]["Altitude"] = Takeoffpoint_AltRel
+        self.BaseFile["mission"]["items"][0]["params"][4] = Takeoffpoint_Lat
+        self.BaseFile["mission"]["items"][0]["params"][5] = Takeoffpoint_Lon
+        self.BaseFile["mission"]["items"][0]["params"][6] = Takeoffpoint_AltRel
+        self.BaseFile["mission"]["plannedHomePosition"][0] = Takeoffpoint_Lat
+        self.BaseFile["mission"]["plannedHomePosition"][1] = Takeoffpoint_Lon
+        self.BaseFile["mission"]["plannedHomePosition"][2] = Takeoffpoint_AltRel
+    def AddSpeedCommand(self,Speed):
+        SpeedCommand = {
+                "autoContinue":True,
+                "command":178,
+                "doJumpId":self.GetItemNum()+1,
+                "frame":2,
+                "params":[
+                    1,
+                    Speed,
+                    -1,
+                    0,
+                    0,
+                    0,
+                    0
+                ],
+                "type":"SimpleItem"
+            }
+        self.BaseFile["mission"]["items"].append(SpeedCommand)
+        pass
+    def AddWaypoint(self,Waypoint_Lat,Waypoint_Lon,Waypoint_AltRel,Waypoint_Speed = 5):
+        WaypointCommand = {
+                "AMSLAltAboveTerrain":None,
+                "Altitude":Waypoint_AltRel,
+                "AltitudeMode":1,
+                "autoContinue":True,
+                "command":16,
+                "doJumpId":self.GetItemNum()+1,
+                "frame":3,
+                "params":[
+                    0,
+                    0,
+                    0,
+                    None,
+                    Waypoint_Lat,
+                    Waypoint_Lon,
+                    Waypoint_AltRel
+                ],
+                "type":"SimpleItem"
+            }
+        self.BaseFile["mission"]["items"].append(WaypointCommand)
+        #self.AddSpeedCommand(Waypoint_Speed)
+    
+    def AddLandtoTakeoffPointCommand(self):
+        LandtoTakeoffPointCommand = {
+                "autoContinue":True,
+                "command":20,
+                "doJumpId":self.GetItemNum()+1,
+                "frame":2,
+                "params":[
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0,
+                    0
+                ],
+                "type":"SimpleItem"
+            }
+        self.BaseFile["mission"]["items"].append(LandtoTakeoffPointCommand)
+        
+    def GenerateFile(self,TargetFileName):
+        with open(TargetFileName,"w+",encoding = "utf-8") as f:
+            f.write(json.dumps(self.BaseFile))
+            #print(self.BaseFile)
+
+        
+
+
+if __name__ == "__main__":
+    plangenerater = PlanGenerater()
+    plangenerater.ChangeTakeoffPoint(51,114,4)
+    plangenerater.AddWaypoint(51.004000144,114.05000012,53)
+    plangenerater.AddWaypoint(51.000000204,114.02000012,56)
+    plangenerater.AddWaypoint(51.006000454,114.00000000,30)
+    plangenerater.AddLandtoTakeoffPointCommand()
+    plangenerater.GenerateFile("generatedplan.plan")
+    
